0c1cfb5ae0133121a79c731368dc24de1d0ecaaf
Jose Agostinho Renner <agostinhorenner@hotmail.com>
2025-10-15 21:27:59 -0300

    [checkpoint]-tratados-valores-api-forecast

--- backend/flaskr/controllers/open_weather.py
+++ backend/flaskr/controllers/open_weather.py
@@ -7,5 +7,5 @@ bp = Blueprint('weather', __name__, url_prefix='/weather')
 
 @bp.route('/', methods=(['POST']))
 def consulta_clima_cidade():
-    data = request.get_json()
+    #data = request.get_json()
     return open_weather_service.consulta_clima_cidade('Blumenau')
--- backend/flaskr/services/open_weather_service/open_weather_service.py
+++ backend/flaskr/services/open_weather_service/open_weather_service.py
@@ -1,18 +1,33 @@
+from datetime import datetime
+from flask import jsonify
 import requests
 import os
 
-
 def consulta_clima_cidade(cidade: str):
-    base_url = os.environ['OPEN_WEATHER_BASE_URL']
+    base_url = os.environ["OPEN_WEATHER_BASE_URL"]
     url_query = f"?q={cidade}"
     appid = f"&appid={os.environ['OPEN_WEATHER_API_KEY']}"
 
     url = base_url + url_query + appid
+    print(url)
 
     try:
         response = requests.get(url)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
+        print(e)  # aqui faria o registro de log em um sistema completo
+        return "Cidade informada não encontrada."
 
-    return response.json()
+    dados_clima = response.json()
+    dados_clima['list'] = get_clima_por_data(dados_clima['list'])
+
+    return jsonify(dados_clima)
+
+
+def get_clima_por_data(lista_datas: tuple) -> list:
+    lista_limpa = []
+    for item in lista_datas:
+        data_afericao = datetime.fromtimestamp(item['dt'])
+        if data_afericao.hour >= 21:
+            lista_limpa.append(item)
+    return lista_limpa
